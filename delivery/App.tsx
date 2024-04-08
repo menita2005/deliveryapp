97341d801841d053161b2ea667bec88897d0a2b3
Sebastian <128631661+menita2005@users.noreply.github.com>
2024-04-07 20:06:18 -0500

    feat(frontend): agregando vista home

--- delivery/App.tsx
+++ delivery/App.tsx
@@ -1,239 +1,41 @@
-import { StatusBar } from 'expo-status-bar';
-import { RoundedButton } from './src/components/RoundedButton';
-import { StyleSheet, Text, View, Image, TextInput, Button, ToastAndroid } from 'react-native';
+import * as React from 'react';
 
-export default function App() {
+import {NavigationContainer} from '@react-navigation/native';
+
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+
+import { HomeScreen } from './src/views/home/home';
+
+const Stack = createNativeStackNavigator();
+
+const App = () => {
 
 return (
 
-<View style={styles.container}>
+<NavigationContainer>
 
-<Image
+<Stack.Navigator screenOptions={{
 
-source={require('./assets/chef.jpg')}
+headerShown: false
 
-style={styles.imageBackground}
+}}>
 
-/>
+<Stack.Screen
 
-<View style={styles.logoContainer}>
+name="HomeScreen"
 
-<Image
-
-source={require('./assets/logo.png')}
-
-style={styles.logoImage}
+component={HomeScreen}
 
 />
 
-<Text style={styles.logoText}>FOOD APP</Text>
+{ /*<Stack.Screen name="Profile" component={ProfileScreen} /> */}
 
-</View>
+</Stack.Navigator>
 
-<View style={styles.form}>
-
-<Text style={styles.formText}>INGRESAR</Text>
-
-<View style={styles.formInput}>
-
-<Image style={styles.formIcon}
-
-source={require('./assets/email.png')}
-
-/>
-
-<TextInput
-
-style={styles.formTextInput}
-
-placeholder='Correo electrónico'
-
-keyboardType='email-address'
-
-/>
-
-</View>
-
-<View style={styles.formInput}>
-
-<Image style={styles.formIcon}
-
-source={require('./assets/password.png')}
-
-/>
-
-<TextInput
-
-style={styles.formTextInput}
-
-placeholder='Contraseña'
-
-keyboardType='default'
-
-secureTextEntry={true}
-
-/>
-
-</View>
-
-<View style={{ marginTop: 30 }}>
-
-<RoundedButton text='ENTRAR' onPress={ () => ToastAndroid.show('HOLA!', ToastAndroid.SHORT)}/>
-
-</View>
-
-<View style={styles.formRegister}>
-
-<Text>¿No tienes cuenta?</Text>
-
-<Text style={styles.formRegisterText}>Regístrate</Text>
-
-</View>
-
-</View>
-
-</View>
+</NavigationContainer>
 
 );
 
-}
+};
 
-const styles = StyleSheet.create({
-
-container: {
-
-flex: 1,
-
-backgroundColor: 'black',
-
-},
-
-imageBackground: {
-
-width: '100%',
-
-height: '100%',
-
-opacity: 0.7,
-
-bottom: '30%',
-
-},
-
-form: {
-
-width: '100%',
-
-height: '40%',
-
-backgroundColor: 'white',
-
-position: 'absolute',
-
-bottom: 0,
-
-borderTopLeftRadius: 40,
-
-borderTopRightRadius: 40,
-
-padding: 30,
-
-},
-
-formText: {
-
-fontWeight: 'bold',
-
-fontSize: 16,
-
-},
-
-formIcon: {
-
-width: 25,
-
-height: 25,
-
-marginTop: 5,
-
-},
-
-formInput: {
-
-flexDirection: 'row',
-
-marginTop: 30,
-
-},
-
-formTextInput: {
-
-flex: 1,
-
-borderBottomWidth: 1,
-
-borderBottomColor: '#AAAAAA',
-
-marginLeft: 15,
-
-},
-
-formRegister: {
-
-flexDirection: 'row',
-
-justifyContent: 'center',
-
-marginTop: 30,
-
-},
-
-formRegisterText: {
-
-fontStyle: 'italic',
-
-color: 'orange',
-
-borderBottomWidth: 1,
-
-borderBottomColor: 'orange',
-
-fontWeight: 'bold',
-
-marginLeft: 10,
-
-},
-
-logoContainer: {
-
-position: 'absolute',
-
-alignSelf: 'center',
-
-top: '15%',
-
-},
-
-logoImage: {
-
-width: 100,
-
-height: 100,
-
-},
-
-logoText: {
-
-color: 'white',
-
-textAlign: 'center',
-
-fontSize: 20,
-
-marginTop: 10,
-
-fontWeight: 'bold',
-
-},
-
-});
+export default App;
