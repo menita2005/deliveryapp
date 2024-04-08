91307cb533113fe709f2a8762edae8c84dbc35b6
Sebastian <128631661+menita2005@users.noreply.github.com>
2024-04-07 19:53:30 -0500

    feat(frontend): agregando un componente RoundedButton

--- delivery/App.tsx
+++ delivery/App.tsx
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
-
-import { StyleSheet, Text, View, Image } from 'react-native';
+import { RoundedButton } from './src/components/RoundedButton';
+import { StyleSheet, Text, View, Image, TextInput, Button, ToastAndroid } from 'react-native';
 
 export default function App() {
 
@@ -15,6 +15,7 @@ source={require('./assets/chef.jpg')}
 style={styles.imageBackground}
 
 />
+
 <View style={styles.logoContainer}>
 
 <Image
@@ -28,7 +29,66 @@ style={styles.logoImage}
 <Text style={styles.logoText}>FOOD APP</Text>
 
 </View>
+
 <View style={styles.form}>
+
+<Text style={styles.formText}>INGRESAR</Text>
+
+<View style={styles.formInput}>
+
+<Image style={styles.formIcon}
+
+source={require('./assets/email.png')}
+
+/>
+
+<TextInput
+
+style={styles.formTextInput}
+
+placeholder='Correo electrónico'
+
+keyboardType='email-address'
+
+/>
+
+</View>
+
+<View style={styles.formInput}>
+
+<Image style={styles.formIcon}
+
+source={require('./assets/password.png')}
+
+/>
+
+<TextInput
+
+style={styles.formTextInput}
+
+placeholder='Contraseña'
+
+keyboardType='default'
+
+secureTextEntry={true}
+
+/>
+
+</View>
+
+<View style={{ marginTop: 30 }}>
+
+<RoundedButton text='ENTRAR' onPress={ () => ToastAndroid.show('HOLA!', ToastAndroid.SHORT)}/>
+
+</View>
+
+<View style={styles.formRegister}>
+
+<Text>¿No tienes cuenta?</Text>
+
+<Text style={styles.formRegisterText}>Regístrate</Text>
+
+</View>
 
 </View>
 
@@ -60,7 +120,7 @@ bottom: '30%',
 
 },
 
-form:{
+form: {
 
 width: '100%',
 
@@ -76,37 +136,104 @@ borderTopLeftRadius: 40,
 
 borderTopRightRadius: 40,
 
-},
-logoContainer:{
+padding: 30,
 
-  position: 'absolute',
-  
-  alignSelf: 'center',
-  
-  top: '15%',
-  
-  },
-  
-  logoImage: {
-  
-  width: 100,
-  
-  height: 100,
-  
-  },
-  
-  logoText: {
-  
-  color: 'white',
-  
-  textAlign: 'center',
-  
-  fontSize: 20,
-  
-  marginTop: 10,
-  
-  fontWeight: 'bold',
-  
-  }
+},
+
+formText: {
+
+fontWeight: 'bold',
+
+fontSize: 16,
+
+},
+
+formIcon: {
+
+width: 25,
+
+height: 25,
+
+marginTop: 5,
+
+},
+
+formInput: {
+
+flexDirection: 'row',
+
+marginTop: 30,
+
+},
+
+formTextInput: {
+
+flex: 1,
+
+borderBottomWidth: 1,
+
+borderBottomColor: '#AAAAAA',
+
+marginLeft: 15,
+
+},
+
+formRegister: {
+
+flexDirection: 'row',
+
+justifyContent: 'center',
+
+marginTop: 30,
+
+},
+
+formRegisterText: {
+
+fontStyle: 'italic',
+
+color: 'orange',
+
+borderBottomWidth: 1,
+
+borderBottomColor: 'orange',
+
+fontWeight: 'bold',
+
+marginLeft: 10,
+
+},
+
+logoContainer: {
+
+position: 'absolute',
+
+alignSelf: 'center',
+
+top: '15%',
+
+},
+
+logoImage: {
+
+width: 100,
+
+height: 100,
+
+},
+
+logoText: {
+
+color: 'white',
+
+textAlign: 'center',
+
+fontSize: 20,
+
+marginTop: 10,
+
+fontWeight: 'bold',
+
+},
 
 });
